c379748463158010032ae06a8648893d5ecb0163
Lucas Batista Gabriel <lucas.bgabriel@gmail.com>
2014-05-06 18:46:26 -0300

    adição do projeto e da aula 9

---
+++ Project/devsoft-project/site/javascripts/main.js
@@ -0,0 +1,12 @@
+// This anonymous function will be executed after the page is loaded.
+$(function() {
+
+
+  // Hide all newly created tokens and show welcome div.
+  $('.getStarted').hide();
+  //$('.getStarted').show();
+
+  // Start animation.
+ // $('.getStarted span').eq(0).fadeIn(200, showNextToken);
+
+});
