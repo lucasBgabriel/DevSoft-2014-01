1d4286c5665f51d4608f49ccfaca7e1516579d80
Lucas Batista Gabriel <lucas.bgabriel@gmail.com>
2014-05-13 17:53:48 -0300

    projeto atualizado

---
+++ Project/devsoft-project/scraper/scraper.rb
@@ -0,0 +1,159 @@
+# encoding:utf-8
+
+require 'mechanize'
+require 'json'
+
+################################################
+##           !USE YOUR CREDENTIALS            ##
+################################################
+USERNAME = '7656147' # Use your username!
+PASSWORD = '2XmA==' # Use your password!
+
+#
+# Helper function that saves a HTML file on the html directory.
+#
+# @param [String] filename the name of the file to be saved.
+# @param [String] body the body of the HTML file.
+#
+def save_html(filename, body)
+  File.open("saved_html/#{filename}.html", "w") do |f|
+    f.write(body.force_encoding('utf-8'))
+  end
+end
+
+def save_json(filename, body)
+  File.open("saved_html/#{filename}.json", "w") do |f|
+    f.write(body.force_encoding('utf-8'))
+  end
+end
+
+mechanize = Mechanize.new
+mechanize.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
+
+mechanize.get('http://estagios.pcs.usp.br/')
+mechanize.get('http://estagios.pcs.usp.br/semLogin/login.aspx')
+
+save_html('before_login', mechanize.page.body)
+
+form = mechanize.page.forms[0]
+
+headers = {
+  'Host' => 'estagios.pcs.usp.br',
+  'Connection'      => 'keep-alive',
+  'Cache-Control'   => 'max-age=0',
+  'Accept'          => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+  'Origin'          => 'http://estagios.pcs.usp.br',
+  'Content-Type'    => 'application/x-www-form-urlencoded',
+  'Referer'         => 'http://estagios.pcs.usp.br/semLogin/login.aspx',
+  'Accept-Encoding' => 'gzip,deflate,sdch',
+  'Accept-Language' => 'en-US,en;q=0.8,pt;q=0.6,de;q=0.4'
+}
+
+params = {
+  '__EVENTTARGET'     => '',
+  '__EVENTARGUMENT'   => '',
+  '__VIEWSTATE'       => form.field_with(name: '__VIEWSTATE').value,
+  '__EVENTVALIDATION' => form.field_with(name: '__EVENTVALIDATION').value,
+  'ctl00$ContentPlaceHolder1$Login1$UserName'    => USERNAME,
+  'ctl00$ContentPlaceHolder1$Login1$Password'    => PASSWORD,
+  'ctl00$ContentPlaceHolder1$Login1$LoginButton' => 'Logar'
+}
+
+mechanize.post("http://estagios.pcs.usp.br/semLogin/login.aspx", params, headers)
+
+save_html('after_login', mechanize.page.body)
+
+################################################
+##         TODO: CONTINUE FROM HERE!          ##
+################################################
+
+selectors = []
+  selectors << '#ContentPlaceHolder1_lblHabilitacao'
+  selectors << '#ContentPlaceHolder1_lblTitulo'
+  selectors << '#ContentPlaceHolder1_lblEmpresa'
+  selectors << '#ContentPlaceHolder1_lblArea'
+  selectors << '#ContentPlaceHolder1_lblDescricao'
+  selectors << '#ContentPlaceHolder1_lblRequisitos'
+  selectors << '#ContentPlaceHolder1_lblBeneficios'
+  selectors << '#ContentPlaceHolder1_lblContatos'
+  selectors << '#ContentPlaceHolder1_lblDataAnuncio'
+  selectors << '#ContentPlaceHolder1_lblDataValidade'
+  selectors << '#ContentPlaceHolder1_lblNumeroVagas'
+
+  my_hash = [] 
+
+  JSON.parse('{
+  "hello": "noes"
+  }')
+
+  emBranco = 0
+  totalVagas = 0
+
+(1329-49).times do |i|
+
+  mechanize.get('http://estagios.pcs.usp.br/aluno/vagas/exibirVaga.aspx?id=' + (i+49).to_s)
+
+  doc = mechanize.page.parser
+  
+  if(doc.css(selectors[8]).text == "")
+    emBranco += 1
+  else
+    my_hash << JSON.parse(['{ 
+      "Habilitacao":'+ doc.css(selectors[0]).text.to_s + ',
+
+      "Titulo":'+ doc.css(selectors[1]).text.to_s + ',
+      
+      "Empresa": '+ doc.css(selectors[2]).text.to_s + ',
+      
+      "Area": '+ doc.css(selectors[3]).text.to_s + ',
+      
+      "Descricao": '+ doc.css(selectors[4]).text.to_s + ',
+      
+      "Requisitos": '+ doc.css(selectors[5]).text.to_s + ',
+      
+      "Beneficios": '+ doc.css(selectors[6]).text.to_s + ',
+      
+      "Contatos": '+ doc.css(selectors[7]).text.to_s + ',
+      
+      "DataAnuncio": '+ doc.css(selectors[8]).text.to_s + ',
+      
+      "DataValidade": '+ doc.css(selectors[9]).text.to_s + ',
+      
+      "NumeroVagas": '+ doc.css(selectors[10]).text.to_s + '
+      }'].to_json).first
+  end
+
+  totalVagas += 1
+end
+
+my_hash << JSON.parse(['{
+  "Total de Vagas":'+ totalVagas.to_s + ',
+  "Vagas em Branco":'+ emBranco.to_s + '
+  }'].to_json).first
+
+puts totalVagas
+puts emBranco
+
+save_json('teste', my_hash.to_json)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Project/devsoft-project/site/javascripts/example.js
+++ Project/devsoft-project/site/javascripts/example.js
@@ -2,6 +2,8 @@
  * Scripts for the example page.
  */
 
+ JSON.open
+
 /*
  * Shows the next token in the welcome message.
  */
@@ -32,3 +34,122 @@ $(function() {
   $('.welcome span').eq(0).fadeIn(200, showNextToken);
 
 });
+
+$(function graphPie() { 
+      // Radialize the colors
+    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function graphPie(color) {
+        return {
+            radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+            stops: [
+                [0, color],
+                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+            ]
+        };
+    });
+    
+    // Build the chart
+        $('#graphPie').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: 'Vagas Analisadas'
+            },
+            tooltip: {
+              pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        },
+                        connectorColor: 'silver'
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Browser share',
+                data: [
+                    ['Validas',    45.0],
+                    ['Duplicadas', 45.0],
+                    {
+                        name: 'Em Branco',
+                        y: 45.0,
+                        sliced: true,
+                        selected: true
+                    },
+                    ['teste', 45.0]
+                ]
+            }]
+        });
+    });
+$(function () {
+        $('#graphColumm').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Vagas por periodo'
+            },
+            subtitle: {
+                text: 'Source: Portal de Estagios -  PCS'
+            },
+            xAxis: {
+                type: 'category',
+                labels: {
+                    rotation: -45,
+                    align: 'right',
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Vagas (millions)'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: 'Vagas: <b>{point.y:.1f}</b>',
+            },
+            series: [{
+                name: 'Population',
+                data: [
+                    ['Shanghai', 23.7],
+                    ['Lagos', 16,1],
+                    ['Instanbul', 14.2],
+                    ['Karachi', 14.0],
+                    ['Mumbai', 12.5],
+                    ['Moscow', 12.1],
+                    ['São Paulo', 11.8], 
+                    ['Lima', 8.9]
+                ],
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    x: 4,
+                    y: 10,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif',
+                        textShadow: '0 0 3px black'
+                    }
+                }
+            }]
+        });
+    });
